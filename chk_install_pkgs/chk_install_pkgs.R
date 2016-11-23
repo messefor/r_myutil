e4f6016708406df9ee7be80785076a068741cd99
messefor <messefor@gmail.com>
2016-11-23 09:09:23 +0900

    Fix typo

--- chk_install_pkgs/chk_install_pkgs.R
+++ chk_install_pkgs/chk_install_pkgs.R
@@ -1,27 +1,26 @@
-
 # check package installed and load them all
-chk.install.pkgs <- function(required.pkg) {
+
+chk.install.pkgs <- function(required.pkgss) {
   
   # @param vector, string/ name of packages to load
   
   # install packages if not installed
-  not.installed <- !(required.pkg %in% installed.packages()[,'Package'])
+  not.installed <- !(required.pkgs %in% installed.packages()[,'Package'])
   if (any(not.installed)) {
-    install.packages(required.pkg[not.installed]) 
+    install.packages(required.pkgs[not.installed]) 
   }
   # load packages  
-  sapply(required.pkg, require, character.only = TRUE)
+  sapply(required.pkgs, require, character.only = TRUE)
 }
 
 
 # @sample
 #
-# required.pkg <- 
+# required.pkgs <-
 #   c(
 #     'dummies',
 #     'tidyverse',
 #     'glmnet'
 #   )
 # 
-# chk.install.pkgs(required.pkg)
-#
+# chk.install.pkgs(required.pkgs)
